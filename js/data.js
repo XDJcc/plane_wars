5d5cc6ef17ec768132eb93d00ec3d6768153c0a1
XDJcc <2045659302@qq.com>
2021-03-10 00:46:21 +0800

    添加继续游戏，新游戏选项

--- js/data.js
+++ js/data.js
@@ -1,9 +1,13 @@
 window.el = document.querySelector(".game-plane");
 window.data = {
 	p : {// 玩家 Player
+		// 玩家的宽
 		w : document.querySelector("#p").offsetWidth,
+		// 玩家的高
 		h : document.querySelector("#p").offsetHeight,
+		// 玩家的边界距离游戏界面水平方向的距离
 		x : el.offsetWidth / 2 - document.querySelector("#p").offsetWidth / 2,
+		// 玩家的边界距离游戏界面垂直方向的距离
 		y : el.offsetHeight - document.querySelector("#p").offsetHeight
 	},
 	
